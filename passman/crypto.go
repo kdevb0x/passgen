518e7254f9ef1c4f9769c3e83d14068e42009fd6
kidoda <kdevb0x@gmail.com>
2018-12-25 19:24:51 -0700

    add LICENSE file

--- passman/crypto.go
+++ passman/crypto.go
@@ -1,20 +1,39 @@
+// Copyright 2018 kdevb0x Ltd. All rights reserved.
+// Use of this source code is governed by the BSD 3-Clause license
+// The full license text can be found in the LICENSE file.
+
 package passman
 
 import (
 	"crypto/aes"
 	"crypto/cipher"
-	"encoding/base64"
+	"crypto/rand"
+	"crypto/sha512"
+	"io"
+	"log"
 
+	"github.com/mr-tron/base58"
 )
 
-type indexer func(*base64.Encoding) index
-
-type index struct {
-	indexNumber int
-	references struct{
-		username string `yaml:`
-	}
+func HashSHA512(key string) string {
+	hsh := sha512.New()
+	hsh.Write([]byte(key))
+	return base58.Encode(hsh.Sum(nil))
 }
-type PassDataBase struct {
-	map[indexer]*record
+
+func encryptAES(data []byte, pass PassString) []byte {
+	blk, err := aes.NewCipher([]byte(HashSHA512(pass.String())))
+	if err != nil {
+		log.Fatalf("error creating encryption cipher: %s", err)
+	}
+
+	gal, err := cipher.NewGCM(blk)
+	nonce := make([]byte, gal.NonceSize())
+	io.ReadFull(rand.Reader, nonce)
+	ctext := gal.Seal(nonce, nonce, data, nil)
+	return ctext
+}
+
+func decryptAES(data []byte, pass PassString) []byte {
+
 }
--- passman/passman.go
+++ passman/passman.go
@@ -1,39 +1,5 @@
+// Copyright 2018 kdevb0x Ltd. All rights reserved.
+// Use of this source code is governed by the BSD 3-Clause license
+// The full license text can be found in the LICENSE file.
+
 package passman
-
-import (
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/rand"
-	"crypto/sha512"
-	"hash"
-	"io"
-	"log"
-	"os"
-
-	"github.com/gonuts/binary"
-	"github.com/mr-tron/base58"
-	"golang.org/x/crypto/pbkdf2"
-)
-
-func HashSHA512(key string) string {
-	hsh := sha512.New()
-	hsh.Write([]byte(key))
-	return base58.Encode(hsh.Sum(nil))
-}
-
-func encryptAES(data []byte, pass PassString) []byte {
-	blk, err := aes.NewCipher([]byte(HashSHA512(pass.String())))
-	if err != nil {
-		log.Fatalf("error creating encryption cipher: %s", err)
-	}
-
-	gal, err := cipher.NewGCM(blk)
-	nonce := make([]byte, gal.NonceSize())
-	io.ReadFull(rand.Reader, nonce)
-	ctext := gal.Seal(nonce, nonce, data, nil)
-	return ctext
-}
-
-func decryptAES(data []byte, pass PassString) []byte {
-
-}
