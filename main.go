74af882885628d3788380cb8bbd6cd9ee2eccba6
Kristofer Davidson <kdevb0x@gmail.com>
2019-05-23 19:26:46 -0700

    display usage when ran without arguments

--- main.go
+++ main.go
@@ -12,35 +12,35 @@ import (
 	"github.com/spf13/pflag"
 )
 
-const usageString = `passgen is a simple, configurable password generator written in Go.
-
-Usage:  passgen [OPTION]... [FILE]...
-
+const usageString = `Usage:  passgen [OPTION]... [FILE]...
+passgen is a simple, configurable password generator.
 
 Available Options:
-    -i, --include {clns}     character GROUPS to include
-    -l, --length [LENGTH]    desired length of output string (defaults to 1)
+       -i, --include {clns}    Character GROUPS to include.
+       -l, --length [LENGTH]   Desired length of output string (defaults to 1).
+       -v, --verify 	       Ensure the generated string includes at least 1
+    				of each catagory indicated by '--include'.
 
-    -v, --verify 	     ensure the generated string includes at least 1 of each catagory indicated by '--include'
+       -q, --quiet 	       Prevent echoing the generated string to the terminal.
+       -o, --output <FILE>     Save output string to FILE.
+       -f, --force 	       If FILE exists, overwrite file instead of appending
+    				to the end (implies -o).
 
-    -q, --quiet 	     prevent echoing the generated string to the terminal
-    -o, --output <FILE>	     save output string to FILE
-    -f, --force 	     if FILE exists, overwrite file instead of appending to the end (implies -o)
-    -h, --help 		     help information (this screen)
+       -h, --help 	       Help information (this screen).
 
 
 GROUPS should be given inline without space or comma seperations (ie: -i lun)
 
 Must be one or more of:
-	    c	    to include capitol (A-Z)
-	    l 	    to include lowercase (a-z)
-	    n 	    to include numbers (0-9)
-	    s 	    to include symbols (!@#$%&*)
+	      	       c    to include capitols [A-Z]
+	      	       l    to include lowercase [a-z]
+	      	       n    to include numbers [0-9]
+	      	       s    to include symbols [!@#$%&*]
 
 Examples:
-	passgen -l 10 -i ln (Generates a 10 char string of characters from [a-z0-9].)
-	passgen -i cls -o pass.txt -q  (Generates a single char from [A-Za-z!@#$%&*] and writes it to the file: 'pass.txt' without echoing it to terminal.)
-	passgen -l 24 -i lcns -v --output=/home/me/totallyNotAPassword.txt  (Generates a 24 char string, verified to include at least 1 from each of: [a-z], [A-Z], [0-9], and [!@#$%&*], writes it to the file: '~/totallyNotAPassword.txt', creating file if necessary, then echoes it to the terminal.)`
+     passgen -l 10 -i ln
+     passgen -i cls -o pass.txt -q
+     passgen -l 24 -i lcns -v --output=/home/me/totallyNotAPassword.txt`
 
 var (
 	usage        = usageString
@@ -62,9 +62,17 @@ func getFlags() {
 	pflag.BoolVarP(&silent, "quiet", "q", false, "suppress echoing of generated string to stdout")
 	pflag.BoolVarP(&verify, "verify", "v", true, "ensure that the generated string includes A͟T͟ ͟L͟E͟A͟S͟T͟ O͟N͟E͟ character from each group passed to [--include, -i]")
 	pflag.Usage = func() {
-		fmt.Print(usage)
+		fmt.Printf("%s\n", usage)
+		os.Exit(1)
 	}
 	pflag.Parse()
+
+	// check if ran without args;
+	// instead of gen and printing a single letter which is useless,
+	// print the usage string and exit.
+	if len(os.Args[:]) <= 1 {
+		pflag.Usage()
+	}
 
 	for i := 0; i < len(includeFlags); i++ {
 		cons := strings.Split(includeFlags[i], "")
