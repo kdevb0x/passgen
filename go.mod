0ea8f9961482e2e2ee05e5e8bf32e7c63fcdebaf
Kristofer Davidson <kdevb0x@gmail.com>
2020-02-11 21:45:45 -0700

    stash to fix bug on master branch

--- go.mod
+++ go.mod
@@ -4,9 +4,7 @@ go 1.12
 
 require (
 	github.com/awnumar/memguard v0.21.0
-	github.com/diskfs/go-diskfs v0.0.0-20190815060803-4a6168b3bc46 // indirect
-	github.com/google/gousb v0.0.0-20190812193832-18f4c1d8a750 // indirect
-	github.com/kevinburke/go-bindata v3.16.0+incompatible // indirect
+	github.com/go-bindata/go-bindata v3.1.2+incompatible
 	github.com/spf13/pflag v1.0.3
 	golang.org/x/crypto v0.0.0-20191122220453-ac88ee75c92c
 	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
--- passman/cmd/simplepwfile/main.go
+++ passman/cmd/simplepwfile/main.go
@@ -5,22 +5,126 @@
 package main
 
 import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"os"
+
+	bd "github.com/go-bindata/go-bindata"
 	"github.com/awnumar/memguard"
 )
 
 const defaultPassLen = 16
 const defaultConstraints = "luns"
 
+
 type fileHandler struct {
-	f *memguard.Stream
+	// internal buffer for enclave io.
+	f           *memguard.LockedBuffer
+	// encrypted enclave of ciphertext.
+	e memguard.Enclave
+	// reports if f has data.
+	hasData bool
+	// reports if enclave is sealed.
+	sealed bool
 }
 
-func NewFileHandler() *fileHandler {
-	return &fileHandler{memguard.NewStream()}
-}
-
-func (h *fileHandler) Add(provider, email, password string) error {
-	if password == "" {
-		password = genPassword(defaultPassLen, defaultConstraints)
+func NewFileHandlerFromFile(file string) *fileHandler {
+	f, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Fatalf("<simplePwFile> error reading existing file %s. %w\n", file, err)
 	}
+	h := &fileHandler{f: memguard.NewBuffer(len(f)), hasData: true}
+	h.f.Move(f)
+	h.f.Freeze()
+	return h
 }
+
+func NewFileHandlerFromInternal() (*fileHandler, error) {
+	f, err := pwTxt()
+	if err != nil {
+		return nil, err
+	}
+	h := &fileHandler{
+		f: memguard.NewBufferFromBytes(f.bytes),
+		hasData: true,
+	}
+	h.f.Melt()
+	h.e = *memguard.NewEnclave(h.f.Bytes())
+
+	return h, nil
+
+}
+
+type password struct {
+	b []byte
+	err string
+}
+
+func (p *password) Error() string {
+	return p.err
+}
+
+func (h *fileHandler) Add(provider, email string, password []byte) error {
+	if len(password) == 0 {
+		return errors.New("must provide password string to add")
+	/*
+
+		err := h.NewPassword(provider, email)
+		if err != nil {
+			return err
+		}
+		password = p
+	}
+	*/
+	if !h.f.Mutable() {
+		h.f.Melt()
+	}
+
+	b := new(bytes.Buffer)
+	if _, err := b.WriteString(provider); err != nil {
+		return err
+	}
+	if _, err := b.WriteString(email); err != nil {
+		return err
+	}
+	if _, err := b.Write(password); err != nil {
+		return err
+	}
+	h.f.Copy(b.Bytes())
+	h.f.Freeze()
+
+	return nil
+}
+
+func (h *fileHandler) ReadCipherTxtFromBin() error {
+	var _ bd.Asset
+	// derive cwd and make temp file for enc/decryption
+	cwd, err := os.Getwd()
+	if err != nil {
+		return fmt.Errorf("error calculating working dir: %w\n", err)
+	}
+
+	tmp, err := ioutil.TempFile(cwd, ".tmp")
+	if err != nil {
+		return fmt.Errorf("unable to create temp file! error: %w\n", err)
+	}
+
+
+
+}
+
+func main() {
+
+}
+/*
+func (h *fileHandler) NewPassword(with string, email string) error {
+	var p password
+	s, err := h.genPassword(defaultPassLen, defaultConstraints)
+}
+
+func (h *fileHandler) genPassword(length int, constraints string) ([]byte, error) {
+	passgen
+}
+
+*/
